dd8fa302d3e3a258fd6e6ccc92208ba0de990224
celeste <lystephen.ca@gmail.com>
2023-04-25 16:15:49 -0700

    - Added Spanish language provider.

--- src/main/java/net/celeste/crescent/CrescentDataGenerator.java
+++ src/main/java/net/celeste/crescent/CrescentDataGenerator.java
@@ -2,6 +2,7 @@ package net.celeste.crescent;
 
 import net.celeste.crescent.data.lang.CrescentEnglishLanguageProvider;
 import net.celeste.crescent.data.CrescentItemModelGenerator;
+import net.celeste.crescent.data.lang.CrescentSpanishLanguageProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
@@ -12,5 +13,6 @@ public class CrescentDataGenerator implements DataGeneratorEntrypoint {
         FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
         pack.addProvider(CrescentItemModelGenerator::new);
         pack.addProvider(CrescentEnglishLanguageProvider::new);
+        pack.addProvider(CrescentSpanishLanguageProvider::new);
     }
 }
